b0e0be50193d4990f3a326edd3a07682954eb03e
王旻伟 <3209304627@qq.com>
2019-10-28 10:55:48 +0800

    feat:✔自建队伍页面

--- lib/pages/Create/Create.dart
+++ lib/pages/Create/Create.dart
@@ -56,34 +56,232 @@ class _CreateState extends State<Create> {
       ),
       body: ListView(
         children: <Widget>[
-          SizedBox(height: 40,),
-          Stack(
-            children: <Widget>[
-              Center(
-                  child: Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  image: user_avatar2,
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 70,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 30,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
                 ),
-              )),
-              // Container(
-              //   height: 1000,
-              //   width: MediaQuery.of(context).size.width,
-              //   color: Colors.black,
-              //   child: Column(
-              //     children: <Widget>[
-              //       // Text("222")
-              //     ],
-              //   ),
-              // )
-            ],
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      image: user_avatar2,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        FontAwesomeIcons.camera,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        print('头像');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          // SizedBox(
-          //   height: 40,
-          // ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(color: Colors.black),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '点击更换辩论队头像',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Table(
+                  columnWidths: {
+                    0: FixedColumnWidth(80),
+                    1: FixedColumnWidth(MediaQuery.of(context).size.width - 80)
+                  },
+                  children: <TableRow>[
+                    TableRow(children: [
+                      Text(
+                        '队名',
+                        style: TextStyle(
+                            color: Colors.grey, fontSize: 20, fontFamily: "楷书"),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '某某辩论队队名',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: "楷书"),
+                      )
+                    ]),
+                    TableRow(children: [
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ]),
+                    TableRow(children: [
+                      Text(
+                        '队伍ID',
+                        style: TextStyle(
+                            color: Colors.grey, fontSize: 20, fontFamily: "楷书"),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '1234566666',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: "楷书",
+                        ),
+                      )
+                    ]),
+                    TableRow(children: [
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ]),
+                    TableRow(children: [
+                      Text(
+                        '成员数',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                          fontFamily: "楷书",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '5',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: "楷书",
+                        ),
+                      )
+                    ]),
+                    TableRow(children: [
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ]),
+                    TableRow(children: [
+                      Text(
+                        '宣言',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                          fontFamily: "楷书",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '最帅最强辩论队',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: "楷书",
+                        ),
+                      )
+                    ]),
+                    TableRow(children: [
+                      SizedBox(height: 10),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ]),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width / 3,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      '邀请好友加入',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width / 3,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                Card(
+                  color: Colors.grey,
+                  child: ListTile(
+                    leading: Container(
+                      decoration: BoxDecoration(
+                        image: user_avatar2,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text('无名'),
+                    // subtitle: Text('无名 请求添加你为好友'),
+                    trailing: FlatButton(
+                      child: Text(
+                        '邀请',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.red,
+                      onPressed: () {},
+                    ),
+                    // isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  color: Colors.grey,
+                  child: ListTile(
+                    leading: Container(
+                      decoration: BoxDecoration(
+                        image: user_avatar,000
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text('某某'),
+                    // subtitle: Text('无名 请求添加你为好友'),
+                    trailing: FlatButton(
+                      child: Text(
+                        '邀请',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.red,
+                      onPressed: () {},
+                    ),
+                    // isThreeLine: true,
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
